0542402a7e1116b5eb93bf8a7d560ab20d0e44bf
harsh diyora <diyoraharsh06@gmail.com>
2021-04-14 16:02:11 +0530

    NA

--- feature_selection_rfe.py
+++ feature_selection_rfe.py
@@ -1,10 +1,28 @@
+'''
+Author: Team NextGen x2020flg@stfx.ca, x2020fli@stfx.ca, x2020fle@stfx.ca
+
+This script does Feature ranking with recursive feature elimination.
+ 
+Output:
+List of features after recursive feature elimination
+
+For tuning RFE refer: https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFE.html
+'''
+
 import pandas as pd
 from sklearn.feature_selection import RFE
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import preprocessing
 
 TRAIN_DATA_FILE_PATH = './train_mapped.csv'
 
 all_train_data = pd.read_csv(TRAIN_DATA_FILE_PATH)
+
+# Convert string values of ID using lable encoder
+le = preprocessing.LabelEncoder()
+train_id =  list(all_train_data.Id.unique())
+le.fit(train_id)
+all_train_data['Id']=le.transform(all_train_data['Id'])
 
 # Splitting input and output
 data_train = [all_train_data["Expected"]]
